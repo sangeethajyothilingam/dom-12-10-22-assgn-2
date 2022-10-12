e6f14301615c99a49b4caeb68ee87c7eff9b12db
sangeethajyothilingam <112925996+sangeethajyothilingam@users.noreply.github.com>
2022-10-12 12:53:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+function foo(){
+    var fname=document.getElementById("first").value;
+    var mname=document.getElementById("middle").value;
+    var lname=document.getElementById("last").value;
+    var res=document.getElementById("email").value;
+    var res1=document.getElementById("pass").value;
+    console.log(fname,mname,lname,res,res1);
+}
